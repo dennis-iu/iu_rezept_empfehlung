e46181bf2160cb867e1879eb0b18a477f4df1c62
dennis-iu <dennis.scholz@iu-study.org>
2025-01-07 12:48:12 +0100

    structure_and_pre-commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Projektpräsentation Rezept-Empfehlungs-Anwendung
+DLBSEPPSD01_D - Projekt: Software Development
+Aufgabenstellung 3 -  Entwicklung einer sonstigen Anwendung
+
---
+++ main.py
@@ -0,0 +1,10 @@
+import logging as log
+
+# log-config
+log.basicConfig(
+    level=log.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+log.info("test run")
